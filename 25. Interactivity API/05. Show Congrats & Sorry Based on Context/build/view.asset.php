38e333192483b717cab03b01494afbccf75aee16
SajidHossainKhan01 <sk6819152@gmail.com>
2026-02-08 16:51:44 +0600

    Upload Show Congrats & Sorry Based on Context

---
+++ 25. Interactivity API/05. Show Congrats & Sorry Based on Context/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '05eec9713ebd0a2a4887', 'type' => 'module');
---
+++ 25. Interactivity API/05. Show Congrats & Sorry Based on Context/src/view.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { store, getContext } from "@wordpress/interactivity"
+
+store("create-block", {
+  actions: {
+    guessAttempt: () => {
+      const context = getContext()
+      if (!context.solved) {
+        if (context.index === context.correctAnswer) {
+          context.showCongrats = true
+          context.solved = true
+        } else {
+          context.showSorry = true
+          setTimeout(() => {
+            context.showSorry = false
+          }, 2600)
+        }
+      }
+    },
+    toggle: () => {
+      const context = getContext()
+      context.isOpen = !context.isOpen
+    }
+  },
+  callbacks: {
+    logIsOpen: () => {
+      const { isOpen } = getContext()
+      // Log the value of `isOpen` each time it changes.
+      console.log(`Is open: ${isOpen}`)
+    }
+  }
+})
