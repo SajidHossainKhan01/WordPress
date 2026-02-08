188c66008b3251f39a5a30a9728d977dd3f33a04
SajidHossainKhan01 <sk6819152@gmail.com>
2026-02-08 16:49:45 +0600

    Upload Rendering HTML With The Interactivity API

---
+++ 25. Interactivity API/03. Rendering HTML With The Interactivity API/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '76d751a6b82d8de94abf', 'type' => 'module');
---
+++ 25. Interactivity API/03. Rendering HTML With The Interactivity API/src/render.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+?>
+
+<?php print_r($attributes) ?>
+
+<div style="background-color: <?php echo $attributes['bgColor'] ?>" class="paying-attention-frontend" data-wp-interactive="create-block" <?php echo wp_interactivity_data_wp_context($attributes) ?>>
+	<p><?php echo $attributes['question'] ?></p>
+	<ul>
+		<template data-wp-each="context.answers">
+			<li data-wp-on--click="actions.guessAttempt" data-wp-text="context.item"></li>
+		</template>
+	</ul>
+</div>
