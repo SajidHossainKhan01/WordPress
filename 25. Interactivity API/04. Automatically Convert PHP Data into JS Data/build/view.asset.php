ae671f220b3ad0e5564b99963600ef0279109cba
SajidHossainKhan01 <sk6819152@gmail.com>
2026-02-08 16:50:47 +0600

    Upload Automatically Convert PHP Data into JS Data

---
+++ 25. Interactivity API/04. Automatically Convert PHP Data into JS Data/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '885d0aa188a7dbfdeae3', 'type' => 'module');
---
+++ 25. Interactivity API/04. Automatically Convert PHP Data into JS Data/src/render.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+	$answers = array();
+	for ($i = 0; $i < count($attributes['answers']); $i++) {
+		$answers[$i]['index'] = $i;
+		$answers[$i]['text'] = $attributes['answers'][$i];
+		$answers[$i]['correct'] = $attributes['correctAnswer'] == $i;
+	}
+	$ourContext = array('answers' => $answers, 'solved' => false, 'showCongrats' => false, 'showSorrry' => false, 'correctAnswer' => $attributes['correctAnswer']);
+
+?>
+
+
+<div style="background-color: <?php echo $attributes['bgColor'] ?>" class="paying-attention-frontend" data-wp-interactive="create-block" <?php echo wp_interactivity_data_wp_context($ourContext) ?>>
+	<p><?php echo $attributes['question'] ?></p>
+	<ul>
+		<?php
+			foreach($ourContext['answers'] as $answer) { ?>
+				<li <?php echo wp_interactivity_data_wp_context($answer) ?> data-wp-on--click="actions.guessAttempt"><?php echo $answer['text'] ?></li>
+			<?php }
+		?>
+	</ul>
+</div>
